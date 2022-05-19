16766f18cdc822e4e9119b90bc5651392daff3c9
Shiv <shivaanshseal@gmail.com>
2022-05-18 20:17:20 -0400

    Added Find three largest numbers

---
+++ Searching/Easy/FindThreeLargestNumbers.py
@@ -0,0 +1,47 @@
+def findThreeLargestNumbers(array):
+    #Name: Find three largest numbers
+	#Category and difficulty: Recursion, Easy
+    #time: O(n)
+	#space: O(1)
+
+	ansList = []
+	firstMax = -99999999
+	secondMax = -99999999
+	thirdMax = -99999999
+	
+	#first pass
+	for i in range(len(array)):
+		if (array[i] > firstMax):
+			firstMax = array[i]
+			firstMaxIndex = i
+	
+	#second pass
+	for j in range(len(array)):
+		if (array[j] > secondMax and j != firstMaxIndex):
+			secondMax = array[j]
+			secondMaxIndex = j
+			
+	#third pass
+	for k in range(len(array)):
+		if (array[k] > thirdMax and k != firstMaxIndex and k != secondMaxIndex):
+			thirdMax = array[k]
+	
+	#add first element to list
+	ansList.append(firstMax)
+	
+	#add second element to list in correct place
+	if(secondMax >= firstMax):
+		ansList.append(secondMax)	
+	else:
+		ansList.insert(0, secondMax)
+	
+	#add third element to list in correct place
+	if(thirdMax <= secondMax and thirdMax <= firstMax):
+		ansList.insert(0, thirdMax)
+	elif((thirdMax <= secondMax and thirdMax >= firstMax) or (thirdMax >= secondMax and thirdMax <= firstMax)):
+		ansList.insert(0, thirdMax)
+	else:
+		ansList.append(thirdMax)
+	
+	return ansList
+    pass
