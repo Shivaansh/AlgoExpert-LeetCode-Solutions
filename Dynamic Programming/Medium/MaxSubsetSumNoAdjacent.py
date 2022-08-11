887542e5ac60eaf79eb51709029dccaa8fd3a893
Shiv <shivaanshseal@gmail.com>
2022-08-10 23:41:18 -0400

    Added revised solution for Max Subset Sum

--- Dynamic Programming/Medium/MaxSubsetSumNoAdjacent.py
+++ Dynamic Programming/Medium/MaxSubsetSumNoAdjacent.py
@@ -4,19 +4,18 @@ def maxSubsetSumNoAdjacent(array):
     #time: O(n)
 	#space: O(1)
 
-    ## Disclaimer: While the code written here is my own creation, the logic / solution reflected in the code is as explained in the video solutions on AlgoExpert.io, for this problem
-
     #Edge cases
     if(len(array) == 0):
-        return 0  
+        return 0
     elif(len(array) == 1):
         return array[0]
 
-    valA = array[0]
-    valB = max(array[0], array[1])
+    sum1 = array[0]
+    sum2 = max(sum1, array[1])
+
     for ind in range(2, len(array)):
-        current = max(valB, valA + array[ind])
-        valA = valB
-        valB = current
-    return valB
+        newSum = max(sum2, sum1+array[ind])
+        sum1 = sum2
+        sum2 = newSum
+    return sum2
 
