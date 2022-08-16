d3b4c610d217463b5500dadeecf61e7ebc0b869a
Shiv <shivaanshseal@gmail.com>
2022-08-15 22:09:38 -0400

    Small update

--- LeetCode Problems/MinimumASCIIDeleteSumForTwoStrings.cpp
+++ LeetCode Problems/MinimumASCIIDeleteSumForTwoStrings.cpp
@@ -5,10 +5,16 @@ public:
         
 //         int text1Size = s1.size();
 //         int text2Size = s2.size();
+//         vector<vector<int>> memo;
+//         vector<int> zeroes(s2.size(), -1);
+//         for(int i = 0; i < s1.size(); i++)
+//         {
+//             memo.push_back(zeroes);
+//         }
 //         return helper(s1, (text1Size-1), s2, (text2Size-1), memo); 
 //     }
     
-//     int helper(string text1, int index1, string text2, int index2)
+//     int helper(string text1, int index1, string text2, int index2, vector<vector<int>> memo)
 //     {
 //         if(text1 == text2) {return 0;}
 //         if(index1 < 0 || index2 < 0)
@@ -32,21 +38,24 @@ public:
 //         }
 //         if(text1[index1] == text2[index2])
 //         {
-//             return helper(text1, index1-1, text2, index2-1);
+//             if(memo[index1][index2] == -1)
+//             {
+//                 memo[index1][index2] =  helper(text1, index1-1, text2, index2-1, memo);
+//             }
+//             return memo[index1][index2];
 //         }
 //         else
 //         {
-//             return min(int(text1[index1]) + helper(text1, index1-1, text2, index2), int(text2[index2]) + helper(text1, index1, text2, index2-1));
+//             if(memo[index1][index2] == -1)
+//             {
+//                 memo[index1][index2] =  min(int(text1[index1]) + helper(text1, index1-1, text2, index2, memo), int(text2[index2]) + helper(text1, index1, text2, index2-1, memo));
+//             }
+//             return memo[index1][index2];
 //         }
 //     }
     //BOTTOM UP
     int minimumDeleteSum(string s1, string s2) {
-        //https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/
-        // Name: Minimum ASCII Delete Sum for Two Strings
-        // Difficulty: Medium
-        // Topic: 2D Dynamic Programming for 2 strings 
-        // Time: O(m * n) for lengths of given strings
-        // Space: O(m * n) for lengths of given strings
+       
         vector<vector<int>> scores;
         vector<int> zeroes(s2.size()+1, 0);
         for(int i = 0; i < s1.size()+1; i++)
