7229546b5dde32d67c5143a654890ba9b9a9742e
Shiv <shivaanshseal@gmail.com>
2022-09-09 19:13:33 -0400

    Added Four Number Sum solutions

---
+++ Arrays/Hard/FourNumberSum.py
@@ -0,0 +1,26 @@
+def fourNumberSum(array, targetSum):
+    #Name: Four Number Sum
+	#Category and difficulty: Arrays, Hard
+    #time: O(n^2) - O(n^3) in average-worst case
+	#space: O(n^2) for size of hashmap
+    quads = []
+    pairMap = {}
+
+    for n1Index in range(1, len(array)-1):
+        num1 = array[n1Index]
+        #forward operations
+        for n2Index in range(n1Index+1, len(array)):
+            num2 = array[n2Index]
+            diff = targetSum - (num1 + num2)
+            if(diff in pairMap):
+                for pair in pairMap[diff]:
+                    quads.append(pair + [num1, num2])
+        #reverse operations
+        for n3Index in range(n1Index):
+            num3 = array[n3Index]
+            if((num1 + num3) not in pairMap):
+                pairMap[num1 + num3] = [[num1, num3]]
+            else:
+                pairMap[num1 + num3].append([num1, num3])
+
+    return quads
