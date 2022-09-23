1f3d5bf9a6e0d2ce779eac6fc0f27e9ca12baefa
Shiv <shivaanshseal@gmail.com>
2022-09-22 21:20:38 -0400

    Updated solution

--- RunTimeChecker.py
+++ RunTimeChecker.py
@@ -1,92 +1,72 @@
 # Import time module
 import time
 ####################### START CODE TO CHECK RUNTIME FOR ##################################    
-def largestRange1(array):
+def longestSubstringWithoutDuplication1(string):
     # Write your code here.
-    #O(n) space
-    hashset = set(array)
-    #O(n) time for both
-    minNum = min(array)
-    maxNum = max(array)
+    charset = set()
 
-    num = minNum
+    maxSubstringLength = float('-inf')
+    maxSubstring = ""
+    start = 0
+    current = start
+    currentLength = 0
 
-    start = minNum
-    end = -1
-    currLength = 0
-    maxLength = -1
-    maxRange = [-1, -1]
-    
-    #O(m) time where m is max num in array
-    while(num <= maxNum):
-        if(num in hashset):
-            currLength += 1
-            end = num
-            num += 1
+    while(current < len(string)):
+        if(string[current] not in charset):
+            charset.add(string[current])
+            currentLength += 1
+            current += 1
         else:
-            if(currLength > maxLength):
-                maxLength = currLength
-                maxRange = [start, end]
-            currLength = 0
-            start = findNextStart(num, maxNum, hashset)
-            num = start
+            if(currentLength > maxSubstringLength):
+                maxSubstringLength = currentLength
+                maxSubstring = string[start:current]
+            currentLength = 0
+            start += 1
+            current = start
+            charset = set()
+
+    if(currentLength > maxSubstringLength):
+        maxSubstring = string[start:current]
         
-    if(currLength > maxLength):
-        maxLength = currLength
-        maxRange = [start, end]
+    return maxSubstring
 
-    return maxRange
-    
-#O(m) time at most
-def findNextStart(num, limit, hashset):
-    for i in range(num+1, limit+1):
-        if(i in hashset):
-            return i
-    return limit+1
-    
-    pass
-
-def largestRange2(array):
+def longestSubstringWithoutDuplication2(string):
     # Write your code here.
+    charTable = {}
 
-    #O(n) space and time
-    hashTable = {}
-    for num in array:
-        #set all numbers to not visited yet
-        hashTable[num] = False
+    maxSubstringLength = float('-inf')
+    maxSubstring = ""
+    start = 0
+    current = start
+    currentLength = 0
 
-    #O(n) time
-    maxLength = -1
-    currLength = 0
-    maxRange = [-1, -1]
-    for num in array:
-        if(hashTable[num]):
-            continue
-        #set num to visited
-        hashTable[num] = True
-        currLength = 1
-        left = num-1
-        right = num+1
-        while(left in hashTable):
-            currLength += 1
-            hashTable[left] = True
-            left -= 1
-        while(right in hashTable):
-            currLength += 1
-            hashTable[right] = True
-            right += 1
-        if(currLength > maxLength):
-            maxLength = currLength
-            maxRange = [left+1, right-1]
+    while(current < len(string)):
+        if(string[current] not in charTable):
+            charTable[string[current]] = current
+            currentLength += 1
+            current += 1
+        else:
+            if(currentLength > maxSubstringLength):
+                maxSubstringLength = currentLength
+                maxSubstring = string[start:current]
+            start = charTable[string[current]]+1
+            currentLength = current-start
+            charTable.clear()
+            for i in range(start, current):
+                if(string[i] not in charTable):
+                    charTable[string[i]] = i
 
-    return maxRange
+    if(currentLength > maxSubstringLength):
+        maxSubstring = string[start:current]
+        
+    return maxSubstring
 
 ####################### END CODE TO CHECK RUNTIME FOR ##################################    
 # record start time
 start = time.time()
  
 # Copy code to check runtime for
-print(largestRange1([1,2,3, 1000001, 1000002, 1000003, 1000004, 1000005, 1000006, 1000007]))
+print(longestSubstringWithoutDuplication1("abacacacaaabacaaaeaaafa"))
 # Mark end time of code
 end = time.time()
  
@@ -98,7 +78,7 @@ print("The time of execution of above program 1 is :",
 start = time.time()
  
 # Copy code to check runtime for
-print(largestRange2([1,2,3, 1000001, 1000002, 1000003, 1000004, 1000005, 1000006, 1000007]))
+print(longestSubstringWithoutDuplication2("abacacacaaabacaaaeaaafa"))
 # Mark end time of code
 end = time.time()
  
--- Strings/Hard/LongestSubstringWithoutDuplication.py
+++ Strings/Hard/LongestSubstringWithoutDuplication.py
@@ -3,32 +3,23 @@ def longestSubstringWithoutDuplication(string):
 	#Category and difficulty: Strings, Hard
     #time: O(n) at max since we go through the string through a window
 	#space: Max O(n) where n is the number of UNIQUE characters in the string 
-    
     charTable = {}
-
-    maxSubstringLength = float('-inf')
     maxSubstring = ""
-    start = 0
-    current = start
-    currentLength = 0
+    startIdx = 0
+    current = 0
 
     while(current < len(string)):
         if(string[current] not in charTable):
             charTable[string[current]] = current
-            currentLength += 1
-            current += 1
         else:
-            if(currentLength > maxSubstringLength):
-                maxSubstringLength = currentLength
-                maxSubstring = string[start:current]
-            start = charTable[string[current]]+1
-            currentLength = current-start
-            charTable.clear()
-            for i in range(start, current):
-                if(string[i] not in charTable):
-                    charTable[string[i]] = i
+            startIdx = max(startIdx, charTable[string[current]]+1)
+        currentSubstring = string[startIdx:current+1]
+        if(len(currentSubstring) > len(maxSubstring)):
+            maxSubstring = currentSubstring
+        charTable[string[current]] = current
+        current += 1
 
-    if(currentLength > maxSubstringLength):
-        maxSubstring = string[start:current]
+    if(len(currentSubstring) > len(maxSubstring)):
+        maxSubstring = currentSubstring
         
     return maxSubstring
