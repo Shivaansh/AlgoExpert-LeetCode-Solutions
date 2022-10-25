c681c98a46b73dc715c0f89e63f0e326975f250f
Shiv <shivaanshseal@gmail.com>
2022-10-25 19:26:47 -0400

    Added iterative preorder and inorder traversals

---
+++ Binary Search Trees/Medium/BSTTraversals_iterative.py
@@ -0,0 +1,48 @@
+#Name: Traverse BST
+#Category and difficulty: BSTs, Medium
+
+def inOrderTraverse(tree, array):
+    #InOrder traversal = left -> parent -> right
+    #time: O(n) where n is number of nodes in tree
+	#space: O(n) for space used by array
+    stack = []
+    temp = tree
+    while(temp is not None or len(stack)>0):
+        if(temp is not None):
+            stack.append(temp)
+            temp = temp.left
+        else:
+            if(len(stack) > 0):
+                temp = stack.pop()
+                array.append(temp.value)
+                temp = temp.right
+    return array
+
+
+def preOrderTraverse(tree, array):
+    #PreOrder traversal = parent -> left -> right
+    #time: O(n) where n is number of nodes in tree
+	#space: O(n) for space used by array
+    stack = []
+    temp = tree
+    while(temp is not None or len(stack)>0):
+        if(temp is not None):
+            array.append(temp.value)
+            stack.append(temp)
+            temp = temp.left
+        else:
+            if(len(stack) > 0):
+                temp = stack.pop()
+                temp = temp.right
+    return array
+
+def postOrderTraverse(tree, array):
+    #PostOrder traversal = left -> right -> parent
+    #time: O(n) where n is number of nodes in tree
+	#space: O(n) for space used by array
+    if(tree is not None):
+        postOrderTraverse(tree.left, array)
+        postOrderTraverse(tree.right, array)
+        array.append(tree.value)
+    return array
+
