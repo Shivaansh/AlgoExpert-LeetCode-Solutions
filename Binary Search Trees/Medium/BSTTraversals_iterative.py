4253573ab5952ee76cd5ec25fa878b67b2d52aa5
Shiv <shivaanshseal@gmail.com>
2022-10-25 19:58:31 -0400

    Added iterative post-order traversal BST

--- Binary Search Trees/Medium/BSTTraversals_iterative.py
+++ Binary Search Trees/Medium/BSTTraversals_iterative.py
@@ -40,9 +40,23 @@ def postOrderTraverse(tree, array):
     #PostOrder traversal = left -> right -> parent
     #time: O(n) where n is number of nodes in tree
 	#space: O(n) for space used by array
-    if(tree is not None):
-        postOrderTraverse(tree.left, array)
-        postOrderTraverse(tree.right, array)
-        array.append(tree.value)
+    stack = []
+    temp = tree
+    while(temp is not None or len(stack)>0):
+        while(temp is not None):
+            if(temp.right is not None):
+                stack.append(temp.right)
+            stack.append(temp)
+            temp = temp.left
+            
+        if(len(stack) > 0):   
+            temp = stack.pop()
+        if(temp is not None and temp.right is not None and len(stack) > 0 and temp.right == stack[-1]):
+            stack.pop()
+            stack.append(temp)
+            temp = temp.right
+        else:
+            array.append(temp.value)
+            temp = None
     return array
 
